85e4bffb55ca47bd20395ea57c3323c94fda544d
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-05-13 22:40:46 +0200

    Add voting logic and update dom element to reflect voting points

--- vote-baby-names.php
+++ vote-baby-names.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
@@ -12,16 +13,44 @@
     Here is your list of names:
   </h1>
 
-  <form action="process-vote.php" method="post">
-      <?php
-      $names = $_POST['names'];
+  <form action="process-vote.php" method="post" id="form">
+    <?php
+    $names = $_POST['names'];
+    $points = array(8, 5, 3);
 
-      foreach ($names as $name) {
-        echo "<input type='checkbox' name='votes[]' value='{$name}'>{$name}";
-      }
-      ?>.
+    foreach ($names as $index => $name) {
+      $point = isset($points[$index]) ? $points[$index] : 0;
+      echo "<div class='name-container'>
+      <input type='checkbox' name='votes[{$name}]' value='{$point}'>{$name}
+      </div>";
+    }
+    ?>
     <input type="submit" value="Vote for Baby Names">
   </form>
+
+  <script>
+    const points = [8, 5, 3];
+    const availablePoints = [...points];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach((checkbox) => {
+      const pointsSpan = document.createElement('span');
+      pointsSpan.classList.add('points');
+      checkbox.parentNode.appendChild(pointsSpan);
+
+      checkbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
+          event.target.value = availablePoints.shift() || 0;
+          pointsSpan.textContent = ` (${event.target.value} points)`;
+        } else {
+          availablePoints.push(event.target.value);
+          availablePoints.sort((a, b) => b - a);
+          pointsSpan.textContent = '';
+          event.target.value = 0;
+        }
+      });
+    });
+  </script>
 
 </body>
 
